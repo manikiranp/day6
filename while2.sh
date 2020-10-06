313e47f528dde25e64505ea089f23263d2cb8603
manikiranp <manikiranp3@gmail.com>
2020-10-06 18:40:27 +0530

    magicnumber

--- while2.sh
+++ while2.sh
@@ -1,20 +1,19 @@
-#!/bin/bash -x
+#!/bin/bash 
 #2
 
+echo "Think of a number between 1 to 100: "
 high=100
 low=1
-echo "Think of a number between 1 to 100: "
-magicnum=$((($high+($low-1))/2))
-while [ $(($magicnum-$low)) -ne  1 ]
+magicnum=50
+while [ $(($high - $low)) -ne  2 ]
 do
-	echo "Enter 1 if less than $magicnum or Enter 0 if greater than $magicnum: "
-	read num
+	read -p "Enter 1 if less than $magicnum or Enter 0 if greater than $magicnum: " num
 	if [ $num -eq 1 ]; then
 	high=$magicnum
 	magicnum=$((($high+$low)/2))
 	else
-	low=$magicnum
-        magicnum=$((($high+$low)/2))
+        low=$magicnum
+	magicnum=$((($high+$low)/2))
 	fi
 done
 echo "Magic number is $magicnum"
